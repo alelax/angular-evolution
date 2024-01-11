421e184b94a186b2c1658aa53b7e51a163b69c60
Alessandro <alessandro.lausdei@gmail.com>
2024-01-11 18:57:39 +0100

    tailwind usage example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,50 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+type Alert = {
+  msg: string;
+  type: 'primary' | 'success' | 'danger'
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   template: `
-    <h1 class="text-3xl font-bold underline">Welcome to {{title}}!</h1>
+    <div class="centred-page sm flex flex-col gap-3">
+      
+      <h1 class="page-title">Alert example</h1>
+      
+      <div
+        class="alert"
+        [ngClass]="{
+        'alert-primary': alert.type === 'primary',
+        'alert-danger': alert.type === 'danger',
+        'alert-success': alert.type === 'success',
+      }"
+      >
+        {{ alert.msg }}
+      </div>
 
-    <router-outlet></router-outlet>
+      <div class="flex gap-3">
+        <button class="btn" (click)="alert = { msg: 'hello 1', type: 'primary' }">primary</button>
+        <button class="btn" (click)="alert = { msg: 'hello 2', type: 'danger' }">danger</button>
+        <button class="btn" (click)="alert = { msg: 'hello 3', type: 'success' }">success</button>
+      </div>  
+    </div>
+    
+    
+    
   `,
-  styles: [],
+  styles: [`
+    
+  `],
 })
 export class AppComponent {
   title = 'angular-demo';
+
+  alert: Alert = {
+    msg: 'hello',
+    type: 'primary'
+  }
 }

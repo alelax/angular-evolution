55158d7208a4a74e1b89edfc844c6473422cb9a2
Alessandro <alessandro.lausdei@gmail.com>
2024-01-14 15:20:46 +0100

    use pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,6 @@ import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
-type Alert = {
-  msg: string;
-  type: 'primary' | 'success' | 'danger'
-}
-
-type Coords = {
-  x: number
-  y: number
-}
-
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -19,11 +9,13 @@ type Coords = {
   template: `
     <div class="centred-page sm flex flex-col gap-3">
       
+      <div>Date 1 {{ today | date: 'dd-MM-yyyy' }}</div>
+      <div>Date 2 {{ timestamp | date: 'hh:mm:ss' }}</div>
       
-      <label>ViewChild and Template Reference Variable</label>
-      <input type="text" (keydown.enter)="keydownHandler()" #input>
+      <div>{{ value | date: '1.2-4' }}</div>
       
-      <button (click)="read(input)">READ VALUE</button>
+      <div>{{ user | json }}</div>
+      <div>{{ users | json }}</div>
       
     </div>
     
@@ -34,26 +26,12 @@ type Coords = {
     
   `],
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent {
 
-  @ViewChild('input') myInput: ElementRef<HTMLInputElement> | undefined;
-
-  ngAfterViewInit(): void {
-    this.myInput?.nativeElement.focus();
-  }
-
-  keydownHandler() {
-
-    const text = this.myInput?.nativeElement.value
-    const urlRegex = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
-    const isUrlValid = text && urlRegex.test(text);
-
-    if (isUrlValid) window.open(text);
-
-  }
-
-  read(input: HTMLInputElement) {
-    console.log(this.myInput?.nativeElement.value)
-  }
+  today = new Date();
+  timestamp = 1202423006;
+  value = 1.1234567890;
+  user = { name: 'Fabio', surname: 'Biondi' };
+  users = [{ name: 'Fabio', surname: 'Biondi' },{ name: 'Fabio', surname: 'Biondi' }];
 
 }

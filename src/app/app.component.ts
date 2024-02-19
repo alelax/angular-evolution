e1908e9d36ed375bed7ec1dca474a0eff2714c68
alelax <alessandro.lausdei@gmail.com>
2024-02-19 17:30:20 +0100

    Test new @if @else

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,24 +10,13 @@ import { RouterOutlet } from '@angular/router';
   template: `
     <div class="centred-page sm">
       
-      <h1 class="page-title">Counter Demo with Signal</h1>
-      
-      <button class="btn" (click)="dec()">-</button>
-      
-      <span 
-        class="text-2xl"
-        [style.color]="isZeroColorOptimized()"
-      >
-        {{ counter() }}
-      </span>
-      
-      <button class="btn" (click)="inc()">+</button>
-      <button class="btn" (click)="reset()" [style.display]="hideIfZero()">reset</button>
-      
-      <div [hidden]="!isZero()">Counter is zero!</div>
-      
-      <!--<div [hidden]="!isZero_old()">Counter is zero OLD!</div>-->
-      <input class="input" type="text" (keydown)="doNothing_old()">
+      @if (logged()) {
+        <h1 class="page-title">Hi Dev</h1>
+        <button class="btn" (click)="logOut()">Logout</button>
+      } @else {
+        <h1 class="page-title">Login</h1>
+        <button class="btn" (click)="singIn()">Login</button>
+      }
 
     </div>    
   `,
@@ -35,95 +24,16 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
 
-  /*
-  * START::Classic-way
-  * */
-  counter_old = 0
+  logged = signal<boolean>(false);
 
-  dec_old() {
-    this.counter_old--;
+  singIn() {
+    this.logged.set(true);
   }
 
-  inc_old() {
-    this.counter_old++
+  logOut() {
+    this.logged.set(false);
   }
 
-  reset_old() {
-    this.counter_old = 0
-  }
-
-  /*
-  * La change detection triggera questo metodi ogni volta
-  * che il template viene renderizzato nuovamente anche
-  * quando non sono dovrebbe. Infatti durante il verificarsi dell'evento
-  * keydown, non dobbiamo verificare che il counter è === 0 o meno eppure
-  * il metodo isZero viene invocato piu volte.
-  * */
-  isZero_old(): boolean {
-    console.log('isZero OLD')
-    return this.counter() === 0;
-  }
-
-  doNothing_old() {
-    console.log('do nothing OLD')
-  }
-  /*
-  * END::Classic-way
-  * */
-
-
-  /*
-  * START::Signal-way
-  * */
-  counter = signal<number>(0);
-
-  constructor() {
-    /*
-    * Per far si che la funzione venga eseguita ogni volta che il signal cambia, è necessario
-    * solamente utilizzare il signal al suo interno.
-    * */
-    effect(() => localStorage.setItem('counter', JSON.stringify(this.counter())));
-  }
-
-  isZero = computed(() => this.counter() === 0 );
-
-  isZeroColor = computed(() => this.counter() === 0 ? 'red' : 'lightgreen');
-
-  /*
-  * Il risultato è lo stesso della computed property "isZeroColor". La differenza sta nel numero di volte che questa computed
-  * viene processata.
-  * Prima, in "isZeroColor" la computed viene processata ogni volta che "this.counter()" cambia, ma in realtà se il counter è
-  * 1, 2, 3 ecc... la condizione sarà sempre vera e non c'è bisogno di rifare la verifica.
-  *
-  * Utilizzando la computed propertu "isZero" anzi che la condizione "this.counter() === 0" la computed property verrà
-  * invocata solo quando la condizione "this.counter() === 0" cambia stato passando da true a false oppure da false a true
-  * ottimizzando così di molto le prestazioni.
-  * */
-  isZeroColorOptimized = computed( () => this.isZero() ? 'red' : 'lightgreen');
-
-  hideIfZero = computed(() => this.counter() === 0 ? 'none' : 'inline');
-
-  dec() {
-    this.counter.update(counter => counter - 1);
-  }
-
-  inc() {
-    /* 
-    * Questo modo porterebbe allo stesso risultato ma non è il metodo corretto 
-    * per modificare il valore di un signal basandosi sul suo valore precedente.
-    * 
-    * this.counter.set(this.counter() + 1) 
-    * 
-    * In questa situazione va utilizzato update()
-    */
-    this.counter.update(counter => counter + 1);
-  }
-
-  reset() {
-    this.counter.set(0);
-  }
-
-  /*
-  * END::Signal-way
-  * */
 }
+
+

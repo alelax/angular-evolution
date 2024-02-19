490bea04221e1bf9d67dce3124cec3638ff404f8
alelax <alessandro.lausdei@gmail.com>
2024-02-19 18:00:28 +0100

    @for and @if

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,14 @@ import { RouterOutlet } from '@angular/router';
 type Product = {
   id: number,
   name: string,
-  cost: number
+  cost: number,
+  description: string
 }
 
 const initialState: Product[] = [
-  {id: 1, name: 'Chocolate', cost: 3},
-  {id: 2, name: 'Milk', cost: 1},
-  {id: 3, name: 'Biscuits', cost: 2}
+  {id: 1, name: 'Chocolate', cost: 3, description: 'Bla bla bla...'},
+  {id: 2, name: 'Milk', cost: 1, description: 'ciao ciaoo...'},
+  {id: 3, name: 'Biscuits', cost: 2, description: 'so gooood....'}
 ]
 
 @Component({
@@ -19,75 +20,31 @@ const initialState: Product[] = [
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   template: `
-    <!-- @if @else -->
-    <div class="centred-page sm">
-      
-      @if (logged()) {
-        <h1 class="page-title">Hi Dev</h1>
-        <button class="btn" (click)="logOut()">Logout</button>
-      } @else {
-        <h1 class="page-title">Login</h1>
-        <button class="btn" (click)="singIn()">Login</button>
-      }
-
-    </div>    
-
-    <!-- @switch e  @empty -->
-    <div class="centred-page sm">
-      @switch (currentStep()) {
-        @case ('step1') {
-          <h1>Step 1</h1>
-          <button class="btn" (click)="currentStep.set('step2')">Next</button>
-        }
-        @case ('step2') {
-          <h1>Step 2</h1>
-          <button class="btn" (click)="currentStep.set('final')">Next</button>
-        }
-        @case ('final') {
-          <h1>Final step</h1>
-        }
-        @default {
-          <h1>Welcome</h1>
-          <button class="btn" (click)="currentStep.set('step1')">Next</button>
-        }
-      }
-    </div>
-
     <!-- @for -->
     <div class="centred-page sm">
-      @for(product of products(); track product.id; let i = $index; let l = $last; let f = $first; let odd = $odd; let even = $even) {
-        <li>{{ i + 1 }} - {{ product.name }}</li>
-      } @empty {
-        <button class="btn" (click)="loadProducts()">Load</button>
-      }
+      @for(product of products(); track product.id) {
+        <button class="btn" (click)="selectProduct(product)">{{ product.name }}</button>
+      } 
 
-      <div *ngIf="hasProducts()">Ci sono {{ totalProducts() }} prodotti</div>
+
+      @if (activeProduct()) {
+        <h1 class="text-2xl">Product Details</h1>
+        <div>€ {{ activeProduct()?.cost }}</div>
+        <div>{{ activeProduct()?.description }}</div>    
+      }
+      
     </div>
   `,
   styles: [``],
 })
 export class AppComponent {
 
-  logged = signal<boolean>(false);
-
-  currentStep = signal<'step1' | 'step2' | 'final' | null>(null);
-
-  products = signal<Product[]>([]);
+  products = signal<Product[]>(initialState);
   
-  hasProducts = computed(() => this.products().length > 0);
+  activeProduct = signal<Product | null>(null);
 
-  totalProducts = computed(() => this.products().length);
-
-  singIn() {
-    this.logged.set(true);
-  }
-
-  logOut() {
-    this.logged.set(false);
-  }
-
-  loadProducts() {
-    this.products.set(initialState)
+  selectProduct(product: Product) {
+    this.activeProduct.set(product);
   }
 
 }

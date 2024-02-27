d6d726e5f3ebc5305aff70fbfc58ca3c92878f2b
alelax <alessandro.lausdei@gmail.com>
2024-02-27 17:28:40 +0100

    timeline component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,33 +3,29 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { PhoneComponent } from './shared/components/phone.component';
 import { UserProfileComponent } from './shared/components/user-profile.component';
+import { TimelineComponent } from './shared/components/timeline.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, PhoneComponent, UserProfileComponent],
+  imports: [CommonModule, RouterOutlet, PhoneComponent, UserProfileComponent, TimelineComponent],
   template: `
     <div class="centered-page sm flex flex-col gap-3">
-      <app-user-profile 
-        [items]="list"
-      />
-
-      <button 
-        class="btn"
-        (click)="inc()"
-      >+</button>
+      <app-timeline [timelineItems]="timelineList"/>
+      <app-timeline [timelineItems]="timelineList" vertical/>
     </div>
   `,
   styles: [``],
 })
 export class AppComponent {
+  timelineList = [
+    { start: '2014', end: 'description' },
+    { start: '2015', end: 'lorem...' },
+    { start: '2018', end: 'bla bla' },
+    { start: '2022', end: 'another' },
+    { start: '2023', end: 'hello' },
+  ]
 
-  list: any = [1, 2, 3]
-
-
-  inc() {
-    this.list = [...this.list, 4]
-  }
   
 
 

d99c932567ce0ba7931756e4c1d2afd246ac744a
alelax <alessandro.lausdei@gmail.com>
2024-03-20 17:16:42 +0100

    component lazy loaded

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,11 @@
 import { Routes } from '@angular/router';
-import { Demo1Component } from './features/demo1/demo1.component';
-import { Demo2Component } from './features/demo2/demo2.component';
-import { Demo3Component } from './features/demo3/demo3.component';
 
 export const routes: Routes = [
-    { path: 'demo1', component: Demo1Component },
-    { path: 'demo2', component: Demo2Component },
-    { path: 'demo3', component: Demo3Component },
+    { path: 'demo1', loadComponent: () => import('./features/demo1/demo1.component').then( file => file.Demo1Component ) },
+    { path: 'demo2', loadComponent: () => import('./features/demo2/demo2.component').then( file => file.Demo2Component ) },
+
+    // Posso rimuovere il then se il componente nella sua classe è esportato as default
+    { path: 'demo3', loadComponent: () => import('./features/demo3/demo3.component') },
     { path: '', redirectTo: 'demo1', pathMatch: 'full' }
 ];
 
